196519f3fba2485df017fbd40f3b2fc1bff371a3
bethelhem920 <nbethelhem920@gmail.com>
2022-10-13 06:58:09 -0700

    second program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
+
 /**
- * main - Prints "with proper grammar, but the outcome
- *                   is a piece of art," followed by a new line.
- * Return: Always 0 (Success)     
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
  */
 int main(void)
 {
