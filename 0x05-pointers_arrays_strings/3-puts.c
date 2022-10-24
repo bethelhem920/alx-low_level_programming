2d020ae2fcfb165959a60e47876096fdc637be05
bethelhem920 <nbethelhem920@gmail.com>
2022-10-24 11:38:55 -0700

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *  * _strlen - returns length of string
- *   * @s: string
- *    * Return: length of string
+ * _puts - prints string
+ *  * @str: string
+ *   * Return: void
  */
 
 void _puts(char *str)
 {
-	int i=0;
+	int i = 0;
 
 	while(str[i] != '\0')
 	{
