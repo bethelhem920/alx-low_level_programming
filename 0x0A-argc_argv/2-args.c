68cc981c6b7fa27b10426792470e8040fe28f3e2
bethelhem920 <nbethelhem920@gmail.com>
2022-11-04 14:01:58 -0700

    all in one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that adds positive numbers
+ * @argc: argument passed from command line
+ * @argv: array that contains the program command line arguments.
+ * Return: returns 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return 0;
+}
