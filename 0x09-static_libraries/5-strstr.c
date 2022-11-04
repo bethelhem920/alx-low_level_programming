f0663ad27d6f86cda80efcb12cfcfceaf0564726
bethelhem920 <nbethelhem920@gmail.com>
2022-11-04 14:35:56 -0700

    hope it works

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -21,11 +21,12 @@ char *_strstr(char *haystack, char *needle)
 		i = 0;
 
 		if (haystack[i] == needle[i])
-		{																		do {
-																					   if (needle[i + 1] == '\0')
-																						   return (haystack);
-																					   i++;
-																				   } while (haystack[i] == needle[i]);
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
 		}
 		haystack++;
 	}
