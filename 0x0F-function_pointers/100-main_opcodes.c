e0d1dc0cde270362f76a220843181df95f2976be
bethelhem920 <nbethelhem920@gmail.com>
2022-11-18 09:27:29 -0800

    message

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,16 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Prints the opcodes of itself.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int bytes, i;
-	char *arr;
+	int (*label)(int, char **) = main;
+	unsigned char opcode;
 
 	if (argc != 2)
 	{
@@ -26,17 +28,20 @@ int main(int argc, char *argv[])
 		exit(2);
 	}
 
-	arr = (char *)main;
-
 	for (i = 0; i < bytes; i++)
 	{
+		opcode = *(unsigned char *)label;
+		printf("%.2x", opcode);
+
 		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
+			continue;
+		printf(" ");
+
+		label++;
 	}
+
+	printf("\n");
+
 	return (0);
 }
 
