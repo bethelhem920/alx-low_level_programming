2be02476fba434b71714bdc41d320fe5c4761d4d
bethelhem920 <nbethelhem920@gmail.com>
2022-11-18 05:56:36 -0800

    message

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list li;
+	va_list list;
 	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(li, n);
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(li, int);
+		sum += va_arg(list, int);
 
-	va_end(li);
+	va_end(list);
 
 	return (sum);
 }
+
