584ff604e8076722b88aa73a33942dc40853d0c2
bethelhem920 <nbethelhem920@gmail.com>
2022-11-29 12:05:17 -0800

    print and count node

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,27 +2,25 @@
 
 /**
  * print_listint- prints all the elements of the list
- * @h: singly linked list head
+ * @h: singly linked list
  *
  * Return: number of nodes
  */
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	int count  = 0;
 
 	if (h == NULL)
 		printf("Linked list empty");
-	else
+
+	const listint_t *i = h;
+
+	while (i != NULL)
 	{
-		const listint_t *i = h;
-		while (i != NULL)
-			{
-				count ++;
-				printf("%d", i->n);
-				printf("\n");
-				i = i->next;
-			}
+		count = count + 1;
+		printf("%d", i->n);
+		i = i->next;
 	}
-	return (count);
-}	
+
+}
