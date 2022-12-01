963228890d54a72765985866a34e90e8a6797c16
bethelhem920 <nbethelhem920@gmail.com>
2022-12-01 13:44:32 -0800

    fixed issues with header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
-
+int _putchar(char c);
 
 
 #endif
