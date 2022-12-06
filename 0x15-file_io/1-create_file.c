fbb015d2c9f4f6340a5b5d877762c417ba101761
bethelhem920 <nbethelhem920@gmail.com>
2022-12-06 14:36:51 -0800

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_file - a function that creats a file
+ *
+ * @filename: the name of the file
+ * @text_content: contents of the file
+ *
+ * Return: returns 1 on SUCCESS and -1 on FAILURE
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+
+	int i;
+
+	int wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fp == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (i = 0; text_content[i]; i++)
+	{
+		wr = write(fp, text_content, i);
+	}
+
+	if (wr == -1)
+		return (-1);
+
+	close(fp);
+
+	return (1);
+}
