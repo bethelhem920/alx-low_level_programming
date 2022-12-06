80d7f115d597a113b16d4961107b2a6ab45fb998
bethelhem920 <nbethelhem920@gmail.com>
2022-12-06 13:58:27 -0800

    read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * read_textfile- reads a text file and prints it
+ *
+ * @letters: a no of letters it should read and print
+ *
+ * Return: actal no of letters it can read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+
+	char *buf[4150];
+	
+	ssize_t count, rd;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	
+	rd = read(fd, buf, letters);
+
+	count = write(STDOUT_FILENO, buf, rd);
+
+	close(fd);
+
+	return (count);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,9 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stddef.h>
+
 #ifndef MAIN_H_
 #define MAIN_H_
 
@@ -5,7 +11,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 
 
 #endif
