af1fe1410f896575895a442de039fd429fea8d57
bethelhem920 <nbethelhem920@gmail.com>
2022-12-07 01:01:47 -0800

    fix null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,13 +25,12 @@ int create_file(const char *filename, char *text_content)
 	if (fp == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	for (i = 0; text_content[i]; i++)
+	if (text_content != NULL)
 	{
-		wr = write(fp, text_content, i);
+		for (i = 0; text_content[i];)
+			i++;
 	}
+	wr = write(fp, text_content, i);
 
 	if (wr == -1)
 		return (-1);
