ee7cd9dde91b5900c8c248cb7a85eaf2d0daef4d
bethelhem920 <nbethelhem920@gmail.com>
2022-12-06 23:47:27 -0800

    appended afile

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (text_content[i] == '\0')
+	while (text_content[i] != '\0')
 	{
 		i++;
 	}
