78a031bfeb2c5d61bd4f895c3f543ee23b1af589
bethelhem920 <nbethelhem920@gmail.com>
2022-12-09 10:48:14 -0800

    can;t find it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,11 +26,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-		text_content = "";
-	
-	for (i = 0; text_content[i];)
+	{
+		close(fp);
+		return (1);
+	}
+	while (text_content[i] != '\0')
 		i++;
-	
+
 	wr = write(fp, text_content, i);
 
 	if (wr == -1)
