4868eaeef21e97bc1d3763b0d5a865f2bc5693be
bethelhem920 <nbethelhem920@gmail.com>
2022-12-09 10:53:36 -0800

    create afile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,18 +20,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fp == -1)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		close(fp);
-		return (1);
-	}
-	while (text_content[i] != '\0')
-		i++;
 
 	wr = write(fp, text_content, i);
 
